9bd9bc521ab986cc9070731b1916241342b5fe72
Breno Amaral <contato.brenoamaral.s@gmail.com>
2024-11-04 15:57:49 -0300

    Organizando o 'MainTeste'

--- compressorImages/MainTeste.java
+++ compressorImages/MainTeste.java
@@ -138,40 +138,8 @@ public class MainTeste {
                     default -> {
                         System.out.println("Opção inválida");
                     }
-            }} while (true);
-
-            // // Caminhos dos arquivos
-            // String inputImagePath = "./images/testImages/11834815.png";
-            // String compressedFilePath = "./images/compressedImages/imagemComprimida.txt";
-            // String outputImagePath = "./images/decompressedImages/ImagemDescomprimida.png";
-    
-            // // Compressão
-            // Compressor compressor = new Compressor();
-            // compressor.compressImage(inputImagePath, compressedFilePath);
-    
-            // // Descompressão
-            // Decompressor decompressor = new Decompressor();
-            // decompressor.decompressImage(compressedFilePath, outputImagePath);
-    
-            // // Cálculo da entropia total
-            // System.out.println("Entropia total da imagem original: " + EntropyCalculator.getTotalEntropy(inputImagePath));
-            // System.out.println("Entropia total da imagem descomprimida: " + EntropyCalculator.getTotalEntropy(outputImagePath));
-    
-            // System.out.println("\n");
-            
-            // // Cálculo da entropia média
-            // System.out.println("Entropia média da imagem original: " + EntropyCalculator.getEntropyImageAverage(inputImagePath));
-            // System.out.println("Entropia média da imagem descomprimida: " + EntropyCalculator.getEntropyImageAverage(outputImagePath));
-            
-            // System.out.println("\n");
-    
-            // // Cálculo da entropia por canal
-            // System.out.println("Entropia da imagem original por canal: \n");
-            // EntropyCalculator.calculate(inputImagePath);
-    
-            // System.out.println("Entropia da imagem descomprimida por canal: \n");
-            // EntropyCalculator.calculate(outputImagePath); 
-}
+                }} while (true);
+    }
 
     public static int Menu() {
         System.out.println("1 - Compressão");
@@ -187,3 +155,35 @@ public class MainTeste {
         return opcao;
     }
 }
+
+// // Caminhos dos arquivos
+// String inputImagePath = "./images/testImages/11834815.png";
+// String compressedFilePath = "./images/compressedImages/imagemComprimida.txt";
+// String outputImagePath = "./images/decompressedImages/ImagemDescomprimida.png";
+    
+// // Compressão
+// Compressor compressor = new Compressor();
+// compressor.compressImage(inputImagePath, compressedFilePath);
+    
+// // Descompressão
+// Decompressor decompressor = new Decompressor();
+// decompressor.decompressImage(compressedFilePath, outputImagePath);
+    
+// // Cálculo da entropia total
+// System.out.println("Entropia total da imagem original: " + EntropyCalculator.getTotalEntropy(inputImagePath));
+// System.out.println("Entropia total da imagem descomprimida: " + EntropyCalculator.getTotalEntropy(outputImagePath));
+    
+// System.out.println("\n");
+            
+// // Cálculo da entropia média
+// System.out.println("Entropia média da imagem original: " + EntropyCalculator.getEntropyImageAverage(inputImagePath));
+// System.out.println("Entropia média da imagem descomprimida: " + EntropyCalculator.getEntropyImageAverage(outputImagePath));
+            
+// System.out.println("\n");
+    
+// // Cálculo da entropia por canal
+// System.out.println("Entropia da imagem original por canal: \n");
+// EntropyCalculator.calculate(inputImagePath);
+    
+// System.out.println("Entropia da imagem descomprimida por canal: \n");
+// EntropyCalculator.calculate(outputImagePath); 
